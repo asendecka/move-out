2cade4122dd8c83b5bc29862ccb0dd0c0a1ea5eb
Aleksandra Sendecka <asendecka@hauru.eu>
2015-01-26 23:05:45 +0100

    Default fake value for SECRET_KEY for Travis

--- move_out/settings.py
+++ move_out/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'super_secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
